6d59afd3ef9813746020a4170067c36c0e02dc55
IZULUZ <dlwm4262@gmail.com>
2026-02-28 11:06:01 +0900

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyDHG3uB-tqoE-4YLBHHvBRM4YhPh9qa-mI",
+  authDomain: "handsup-262b8.firebaseapp.com",
+  databaseURL: "https://handsup-262b8-default-rtdb.firebaseio.com",
+  projectId: "handsup-262b8",
+  storageBucket: "handsup-262b8.firebasestorage.app",
+  messagingSenderId: "494093439378",
+  appId: "1:494093439378:web:af0a9081d72f021031f14b"
+};
+
+firebase.initializeApp(firebaseConfig);
+var db = firebase.database();
